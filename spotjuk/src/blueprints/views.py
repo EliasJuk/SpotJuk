51ff417f1509d9ace96799e7d1be02c818c0abeb
EliasJuk <eliasp.juk@hotmail.com>
2021-12-27 22:19:56 -0300

    carregar albums e capa

--- spotjuk/src/blueprints/views.py
+++ spotjuk/src/blueprints/views.py
@@ -3,7 +3,41 @@ from flask import render_template
 def init_app(app):
   @app.route('/')
   def index():
-    return render_template('index.html')
+    albums = [
+      {
+        'artista':'Artista-01',
+        'capa': 'capa-01.jpg'
+      },
+      {
+        'artista':'Artista-02',
+        'capa': 'capa-02.jpg'
+      },
+      {
+        'artista':'Artista-03',
+        'capa': 'capa-03.jpg'
+      },
+      {
+        'artista':'Artista-04',
+        'capa': 'capa-04.jpg'
+      },
+      {
+        'artista':'Artista-05',
+        'capa': 'capa-05.jpg'
+      },
+      {
+        'artista':'Artista-06',
+        'capa': 'capa-06.jpg'
+      },
+      {
+        'artista':'Artista-07',
+        'capa': 'capa-07.jpg'
+      },
+      {
+        'artista':'Artista-08',
+        'capa': 'capa-08.jpg'
+      }
+    ]
+    return render_template('index.html', albums=albums)
 
   @app.route('/login')
   def login():
